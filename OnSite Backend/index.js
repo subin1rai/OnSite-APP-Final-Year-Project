6daaaec58f718f89be081c576bc11f60109af464
MR. Subi <bantawasubin@gmail.com>
2024-11-22 22:38:43 +0545

    backend setup

---
+++ OnSite Backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const routes = require("./routes/routes");
+require("dotenv").config();
+const app = express();
+
+
+app.use(express.json());
+
+// Use the routes
+app.use("/api", routes);
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ OnSite Backend/routes/routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// // Authentication routes
+// router.post("/signup", authController.signup);
+// router.post("/login", authController.login);
+// router.post("/logout", authController.logout);
+
+
+module.exports = router;
