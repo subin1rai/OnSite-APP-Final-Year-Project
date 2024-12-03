094e099f497e885bb85f7ce49555a1ffcd603017
MR. Subi <bantawasubin@gmail.com>
2024-12-03 22:23:01 +0545

    Backend:login function

--- OnSite Backend/controllers/authController.js
+++ OnSite Backend/controllers/authController.js
@@ -47,8 +47,43 @@ const signUp = async(req,res)=>{
             token
         });
     } catch (error) {
+        console.log(error);
         res.status(500).json({message:"Internal server error",error:error.message});
     }
 }
 
-module.exports = {signUp};
+const login = async(req,res)=>{
+    try {
+        const {email,password}= req.body;
+        if(!email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        const user = await prisma.user.findFirst({
+            where:{
+                email:email
+            }
+        })
+        if(!user){
+            return res.status(400).json({message:"User not found"});
+        }
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            return res.status(400).json({message:"Invalid password"});
+        }
+        const token = jwt.sign(
+            { userId: user.id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '2s' }
+        );
+        res.status(200).json({
+            message:"Login successful",
+            user,
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Internal server error",error:error.message});
+    }
+}
+
+module.exports = {signUp,login};
