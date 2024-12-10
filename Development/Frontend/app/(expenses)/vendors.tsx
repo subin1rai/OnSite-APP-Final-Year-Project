e1932116558d11b7b19d8b8c6c7c90cbfc788d7a
MR. Subi <bantawasubin@gmail.com>
2024-12-10 21:04:21 +0545

    Frontend: 💄 Vendor UI card complete

--- Development/Frontend/app/(expenses)/vendors.tsx
+++ Development/Frontend/app/(expenses)/vendors.tsx
@@ -1,16 +1,31 @@
+import { images } from '@/constants';
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, ScrollView, Text, View } from 'react-native'
 
 const Vendors = () => {
   return (
-    <View>
-      <Text>Vendors</Text>
-      <Text>Vendors</Text>
-      <Text>Vendors</Text>
-      <Text>Vendors</Text>
-      <Text>Vendors</Text>
-      <Text>Vendors</Text>
+    <ScrollView>
+    {/* <View className="bg-white px-4 py-2 "> */}
+    <View className="flex flex-row bg-white justify-between items-center px-4 mx-4 rounded-md mt-2 py-2">
+      <View className="gap-4 py-3 flex-row items-center">
+        <View>
+          <Image source={images.imageProfile}/>
+        </View>
+        <View className="">
+          <View>
+          <Text className="font-semibold text-2xl">AK Breakes</Text>
+          </View>
+          <View>
+          <Text className="font-normal text-md text-[#3C3C43]">9810439543</Text>
+          </View>
+        </View>
+       
+      </View>
+      <View className="">
+        <Text className="text-[16px] font-medium text-[#FF3B30]">NRP. 50000</Text>
+      </View>
     </View>
+    </ScrollView>
   )
 }
 
