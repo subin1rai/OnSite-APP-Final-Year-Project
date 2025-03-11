fdaf52275d8d4f3204119419bb2362e5ddeb27a8
subin1rai <bantawasubin@gmail.com>
2025-03-11 21:37:52 +0545

    Frontend: Header fixed and made the UI for uploading the files

---
+++ Development/Frontend/Components/AddFile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const AddFile = () => {
+  return (
+    <View>
+      <Text>Good</Text>
+    </View>
+  )
+}
+
+export default AddFile
--- Development/Frontend/app/(project)/_layout.tsx
+++ Development/Frontend/app/(project)/_layout.tsx
@@ -1,49 +1,40 @@
 import React from "react";
 import { Stack } from "expo-router";
 import { StatusBar, TouchableOpacity } from "react-native";
-import { Ionicons } from "@expo/vector-icons"; // Import back button icon
+import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import "../../global.css";
 import Toast from "react-native-toast-message";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import eventBus from "../../context/eventBus";
 
 const Layout = () => {
   const router = useRouter();
 
   return (
-        <GestureHandlerRootView className="flex-1">
+    <GestureHandlerRootView className="flex-1">
       <StatusBar barStyle="dark-content" backgroundColor="white" translucent={false} />
       <Stack>
         <Stack.Screen
           name="create_project"
           options={{
             title: "Create Project",
-            headerStyle: {
-              backgroundColor: "#FDB541", 
-            },
-            headerTitleStyle: {
-              fontSize: 20,
-              fontWeight: "semibold",
-              color: "#fff", 
-            },
+            headerStyle: { backgroundColor: "#FDB541" },
+            headerTitleStyle: { fontSize: 20, fontWeight: "semibold", color: "#fff" },
             headerTintColor: "#fff",
             headerLeft: () => (
-              <TouchableOpacity
-                onPress={() => router.back()} 
-                style={{ marginLeft: 15 }}
-              >
+              <TouchableOpacity onPress={() => router.back()} style={{ marginLeft: 15 }}>
                 <Ionicons name="arrow-back" size={24} color="white" />
               </TouchableOpacity>
             ),
           }}
         />
-        <Stack.Screen  name="project_home" options={{ headerShown: false }}/>
-
+        <Stack.Screen
+          name="all_files" options={{ headerShown: false }}  />
+        <Stack.Screen name="project_home" options={{ headerShown: false }} />
       </Stack>
-
       <Toast />
     </GestureHandlerRootView>
-
   );
 };
 
